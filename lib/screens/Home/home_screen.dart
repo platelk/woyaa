4ce5a8671669f866ab761ecfec47b1b592f4ff31
Yoann Deshaies <38348888+joelatruite@users.noreply.github.com>
2023-05-02 20:52:00 +0200

    good path for Unknown picture et Hyperlinks

--- lib/screens/Home/home_screen.dart
+++ lib/screens/Home/home_screen.dart
@@ -128,7 +128,9 @@ class HomeScreen extends StatelessWidget {
                                         color: Colors.white)),
                           ],
                         ),
-                        Row(
+                        GestureDetector(
+                          onTap: () => html.window.open('https://goo.gl/maps/wLz2nsx7YJLwbwkR8', 'new tab'),
+                          child : Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text("Château du Bois-Guy",
@@ -139,8 +141,10 @@ class HomeScreen extends StatelessWidget {
                                         fontWeight: FontWeight.bold,
                                         color: Colors.white)),
                           ],
-                        ),
-                        Row(
+                        )),
+                        GestureDetector(
+                          onTap: () => html.window.open('https://rickroll.it/rickroll.mp4', 'new tab'),
+                          child : Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text("Parigné, Bretagne",
@@ -151,7 +155,7 @@ class HomeScreen extends StatelessWidget {
                                         fontWeight: FontWeight.bold,
                                         color: Colors.white)),
                           ],
-                        ),
+                        )),
                         const Spacer(),
                         Row(
                           children: [
